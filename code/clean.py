7944bc5dd5cd958930b2daf80dce17e802eae1b6
annehs <annehoogerduijn@gmail.com>
2020-04-06 15:46:10 +0200

    Added pie chart + plots folder

--- code/clean.py
+++ code/clean.py
@@ -1,9 +1,12 @@
 import sys
 import os
 import pandas as pd
-
+import matplotlib.pyplot as plt
+import numpy as np
 '''
 Clean
+
+For the Programme column
 '''
 
 fileName='ODI-2020.csv'
@@ -109,32 +112,65 @@ for i in progData.index:
     elif any(substr in progData.loc[i] for substr in HLTstring):
         progData.loc[i] = 'Human Language Technology'
 
+    # Mechanical Engineering
+    elif 'Mechenaical' in progData.loc[i]:
+        progData.loc[i] = 'Mechanical Engineering'
+
     # Exchange
     elif any(substr in progData.loc[i] for substr in EXCstring):
         progData.loc[i] = 'Exchange'
 
+    # Unknown
+    if progData.loc[i] == 'MSc':
+        progData.loc[i] = 'Unknown'
 
     # Remove title & University
     progData.loc[i]=progData.loc[i].replace('Master ', '')
     progData.loc[i]=progData.loc[i].replace('MSc ', '')
     progData.loc[i]=progData.loc[i].replace(' UVA', '')
 
-    if progData.loc[i] == 'MSc':
-        progData.loc[i] = 'Unknown'
 
-
-# List of unique column entries
-progList=[]
+# Dictionary (key=programme, value=nr of students)
+progDict={}
 for entry in progData:
-    if entry not in progList:
-        progList.append(entry)
-for entry in progList:
-    print(entry)
+    if entry in progDict:
+        progDict[entry]+=1
+    else:
+        progDict[entry]=1
 
-print(len(progList))
-print()
+# Plot pie chart
+# Documentation: https://matplotlib.org/3.1.1/gallery/pie_and_polar_charts/pie_and_donut_labels.html
+fig, ax = plt.subplots(figsize=(12, 6), subplot_kw=dict(aspect="equal"))
 
-# Save to csv
+labels = [(str(prog) + " (" + str(progDict[prog]) + ")") for prog in progDict]
+data = [amount for amount in progDict.values()]
+
+wedges, texts = ax.pie(data, wedgeprops=dict(width=0.5), startangle=-40)
+
+bbox_props = dict(boxstyle="square,pad=0.3", fc="w", ec="k", lw=0.72)
+kw = dict(arrowprops=dict(arrowstyle="-"),
+          bbox=bbox_props, zorder=0, va="center")
+
+for i, p in enumerate(wedges):
+    ang = (p.theta2 - p.theta1)/2. + p.theta1
+    y = np.sin(np.deg2rad(ang))
+    x = np.cos(np.deg2rad(ang))
+    horizontalalignment = {-1: "right", 1: "left"}[int(np.sign(x))]
+    connectionstyle = "angle,angleA=0,angleB={}".format(ang)
+    kw["arrowprops"].update({"connectionstyle": connectionstyle})
+    ax.annotate(labels[i], xy=(x, y), xytext=(1.35*np.sign(x), 1.4*y),
+                horizontalalignment=horizontalalignment, **kw, size=14)
+
+# Save pie chart
+filePath='..//plots//'
+fileDir = os.path.dirname(os.path.realpath('__file__'))
+plotPath = os.path.join(fileDir, filePath) #'../data/test.csv')
+plotPath = os.path.abspath(os.path.realpath(plotPath))
+plotName = 'MscProgrammesPieChart.png'
+plt.savefig(os.path.join(plotPath, plotName))
+plt.show()
+
+# Save cleaned data to csv
 csvName='cleaned.csv'
 filePath = '..//data//' + csvName
 fileDir = os.path.dirname(os.path.realpath('__file__'))
