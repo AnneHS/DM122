3001bf028adb8b0ff3f29802eb2574dcfa212bef
annehs <annehoogerduijn@gmail.com>
2020-04-11 14:38:44 +0200

    Some minor changes to the wordcloud in clean.py

--- code/clean.py
+++ code/clean.py
@@ -144,7 +144,7 @@ for i in progData.index:
 
     elif 'medical informatics'in progData.loc[i]:
         progData.loc[i] = 'Medical Informatics'
-        
+
     # Exchange
     elif any(substr in progData.loc[i] for substr in EXCstring):
         progData.loc[i] = 'Exchange'
@@ -231,20 +231,33 @@ progData.to_csv(csvPath, header='Programme')
 gd1Data = df['GoodDay1']
 gd2Data = df['GoodDay2']
 
+dayList =[]
+for entry in gd1Data:
+    if entry not in dayList:
+        dayList.append(entry)
+for entry in gd2Data:
+    if entry not in dayList:
+        dayList.append(entry)
+
+for entry in dayList:
+    print(entry)
+print(len(dayList))
+print()
+
 # Concatenate entries
 text1 = " ".join(entry for entry in gd1Data)
 text2 = " ".join(entry for entry in gd2Data)
 text = text1 + text2
 
 # Create and generate a word cloud image
-wordcloud = WordCloud(width=800, height=400, colormap='Pastel1').generate(text)
+wordcloud = WordCloud(width=800, height=400, min_font_size=15, background_color='white', colormap='tab20').generate(text)
 plt.imshow(wordcloud, interpolation='bilinear')
 plt.axis("off")
 
 # Save
 plotName = 'GoodDayWordChart.png'
-plt.savefig(os.path.join(plotPath, plotName))
-#plt.show()
+plt.savefig(os.path.join(plotPath, plotName), bbox_inches='tight')
+plt.show()
 
 stressData = df['StressLvl']
 csvName='stressCleaned.csv'
