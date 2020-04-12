78aa7034c45a1f7575b69b76ed574e2c5d879640
annehs <annehoogerduijn@gmail.com>
2020-04-12 22:13:03 +0200

    cabin data cleaned

--- code/titanic.py
+++ code/titanic.py
@@ -3,6 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 import re
+import math
 
 fileName='train.csv'
 filePath = '..//data//titanic//' + fileName
@@ -15,6 +16,7 @@ csvPath = os.path.abspath(os.path.realpath(csvPath))
 # read data
 df = pd.read_csv(csvPath, sep=',', engine='python')
 
+'''
 # Get passenger total, total deaths, total survived
 survivalData = df['Survived']
 deaths=0
@@ -140,6 +142,7 @@ plotName = 'GenderSurvivalBarChart.png'
 plt.savefig(os.path.join(plotPath, plotName), bbox_inches='tight')
 plt.show()
 
+'''
 
 ageData = df['Age']
 x=[]
@@ -147,3 +150,45 @@ y=[]
 for i, entry in enumerate(ageData):
     if isinstance(entry, int):
         x.app
+
+unknown=0
+cabinData = df['Cabin']
+
+loc = np.where(pd.isnull(cabinData))
+rows=loc[0]
+
+deck_dict={'A':0, 'B':1, 'C':2, 'D':3, 'E':4, 'F':5, 'G':6, 'T':7}
+decks=[]
+numbers=[]
+for i, entry in enumerate(cabinData):
+    if i in rows:
+        decks.append(-1)
+        numbers.append(-1)
+    else:
+        cabins = entry.split()
+        if len(cabins)==1:
+            d=deck_dict[cabins[0][0]]
+            decks.append(d)
+            numbers.append(cabins[0][1:])
+        else:
+            ds=[]
+            ns=[]
+            for cabin in cabins:
+                if cabin[1:]=='':
+                    n=-1
+                else:
+                    n=int(cabin[1:])
+                ds.append(deck_dict[cabin[0]])
+                ns.append(n)
+            decks.append(ds)
+            numbers.append(ns)
+
+d={'Decks':decks, 'Numbers': numbers}
+cabins_df = pd.DataFrame(d)
+
+csvName='cabins_cleaned.csv'
+filePath = '..//data//titanic_cleaned//' + csvName
+fileDir = os.path.dirname(os.path.realpath('__file__'))
+csvPath = os.path.join(fileDir, filePath)
+csvPath = os.path.abspath(os.path.realpath(csvPath))
+cabins_df.to_csv(csvPath)
